533cd264e53283f9fcb4a7f6604cdd3f4e5b09db
IrvanAhmadP <irvan.ap1618@gmail.com>
2022-10-07 20:25:45 +0700

    added new components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,12 @@ import Header from "src/components/Header";
 import Main from "src/components/Main";
 import Modal from "src/components/Modal";
 import Container from "src/components/Container";
-import DataNotFound from "src/components/DataNotFound";
+import DataNotFound from "src/components/NotFound/DataNotFound";
+import NoteNotFound from "src/components/NotFound/NoteNotFound";
 import Note from "src/components/Note";
 import NewNoteButton from "src/components/Form/buttons/NewNoteButton";
 import SimpleButton from "src/components/Form/buttons/SimpleButton";
+import RoundedButton from "src/components/Form/buttons/RoundedButton";
 import Checkbox from "src/components/Form/Checkbox";
 import Input from "src/components/Form/Input";
 import Textarea from "src/components/Form/Textarea";
@@ -19,8 +21,10 @@ export {
   Modal,
   Container,
   DataNotFound,
+  NoteNotFound,
   Note,
   SimpleButton,
+  RoundedButton,
   NewNoteButton,
   Textarea,
   NavBar,
