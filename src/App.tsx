ac769d3853d1933b3bbc6420ccd8287358462a01
IrvanAhmadP <irvan.ap1618@gmail.com>
2022-10-15 20:10:31 +0700

    added login and register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //pages
+import Login from "src/pages/Login";
+import Register from "src/pages/Register";
 import Note from "src/pages/Note";
 import Notes from "src/pages/Notes";
 import NewNote from "src/pages/NewNote";
 import EditNote from "src/pages/EditNote";
 import NotFound from "src/pages/404";
 import SearchNotes from "./pages/SearchNote";
+import { ReactNode } from "react";
 
 function App() {
   const activeNotes = <Notes page="active" />;
@@ -22,9 +25,18 @@ function App() {
         <Route path="edit/:id" element={<EditNote />} />
         <Route path="search" element={<SearchNotes />} />
         <Route path="*" element={<NotFound />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+type ProtectedRouteProps = {
+  children: ReactNode;
+};
+
+function ProtectedRoute({ children }: ProtectedRouteProps) {}
 
 export default App;
