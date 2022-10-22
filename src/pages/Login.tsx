59095c206d8485d554343ddf98d1a9ce91552175
IrvanAhmadP <irvan.ap1618@gmail.com>
2022-10-22 20:21:21 +0700

    Done - added method - update jsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,69 @@
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import AuthLayout from "src/layouts/AuthLayout";
-import { Hr, Input, SimpleButton } from "src/components";
+import { useInput } from "src/hooks/useInput";
+import { login, putAccessToken } from "src/utils/api";
+import { Hr, Loading, Input, SimpleButton } from "src/components";
 
 function Login() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, handleEmailChange] = useInput("");
+  const [password, handlePasswordChange] = useInput("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    document.title = "Login";
+  }, []);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setError("");
+    setIsLoading(true);
+
+    const result = await login({ email, password });
+    if (result.error === true) {
+      setError(result.message);
+    } else {
+      putAccessToken(result.data.accessToken);
+      navigate("/");
+    }
+
+    setIsLoading(false);
+  };
+
   return (
     <AuthLayout>
       <div className="flex w-96 flex-col rounded-md p-8">
         <h1 className="text-center text-2xl font-bold">Login</h1>
-        <Input
-          label="Email"
-          placeholder="Email"
-          value=""
-          handleChange={() => {}}
-        />
-        <Input
-          label="Password"
-          placeholder="Password"
-          value=""
-          handleChange={() => {}}
-        />
+        <form onSubmit={handleLogin}>
+          <Input
+            label="Email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            handleChange={handleEmailChange}
+          />
+          <Input
+            type="password"
+            label="Password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            handleChange={handlePasswordChange}
+          />
 
-        <SimpleButton
-          color="bg-blue-500 hover:bg-blue-600 text-white"
-          classes="py-2 font-semibold mt-2"
-        >
-          Login
-        </SimpleButton>
+          <p className="text-sm text-red-500">{error}</p>
+
+          <SimpleButton
+            classes="mt-2 flex w-full justify-center py-2 font-semibold"
+            color="bg-blue-500 hover:bg-blue-600 text-white"
+          >
+            {isLoading && <Loading classes="mx-2 h-6 w-6" />}
+            Login
+          </SimpleButton>
+        </form>
 
         <Hr classes="mt-3 mb-1" text="OR" />
 
