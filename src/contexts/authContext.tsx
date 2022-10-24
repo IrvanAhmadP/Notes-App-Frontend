c0cd3bae3d52d57de40c0de8a668ba372ced9c28
IrvanAhmadP <irvan.ap1618@gmail.com>
2022-10-24 20:42:42 +0700

    added loading state, onLogin, and onLogout

--- src/contexts/authContext.tsx
+++ src/contexts/authContext.tsx
@@ -8,33 +8,51 @@ import {
 } from "react";
 import { getAccessToken, getUserLogged, putAccessToken } from "src/utils/api";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext({
+  isLoading: true,
+  auth: null,
+  onLogin: (token: string) => {},
+  onLogout: () => {},
+});
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
 function AuthProvider({ children }: AuthProviderProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [token, setToken] = useState(getAccessToken || null);
   const [auth, setAuth] = useState(null);
 
   useEffect(() => {
-    const token = getAccessToken();
-
     if (token !== "" && token !== null) {
       getUserLogged().then(({ error, data }) => {
         if (!error) {
           setAuth(data);
         } else {
-          putAccessToken("");
-          setAuth(data);
+          onLogout();
         }
+
+        setIsLoading(false);
       });
+    } else {
+      setIsLoading(false);
     }
-  }, []);
+  }, [token]);
+
+  const onLogin = (token: string) => {
+    putAccessToken(token);
+    setToken(token);
+  };
+
+  const onLogout = () => {
+    putAccessToken("");
+    setAuth(null);
+  };
 
   const value = useMemo(() => {
-    return auth;
-  }, [auth]);
+    return { isLoading, auth, onLogin, onLogout };
+  }, [isLoading, auth]);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
